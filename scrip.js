dcb1337c387174cc4a8e1804f01450db3b5a3b56
aglkrnti <agilkaruniati@gmail.com>
2024-11-03 12:41:02 +0700

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const buyButton = document.getElementById("buyButton");
+
+  buyButton.addEventListener("click", function () {
+    alert(
+      "Terima kasih telah membeli tiket! Kami akan menghubungi Anda segera."
+    );
+  });
+});
